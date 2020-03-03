c5fcf65baea74132456b89d99c942fdcb2384cfb
paczarny <pawel350812@gmail.com>
2020-03-03 21:12:37 +0100

    add mvn install stage

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,9 +1,18 @@
 pipeline {
-    agent any
+    agent {
+        docker {
+            image "adoptopenjdk/maven-openjdk11"
+        }
+    }
     stages {
         stage("List files") {
             steps {
                 sh "ls ."
+            }
+        }
+        stage("MVN install") {
+            steps {
+                sh "cd simple-backend && mvn clean install"
             }
         }
     }
