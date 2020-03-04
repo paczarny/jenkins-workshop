94d975bde8e100d82eb909bb76fc4a1d4ddfac0f
paczarny <pawel350812@gmail.com>
2020-03-04 14:55:35 +0100

    parameters added

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -24,7 +24,7 @@ pipeline {
         }
         stage("Run jar") {
             steps {
-                sh "cd simple-backend/target && java -jar app.jar --spring.profiles.active=$param.PROFILE --PRODUCTNAME=$param.PRODUCT_NAME"
+                sh "cd simple-backend/target && java -jar app.jar --spring.profiles.active=$params.PROFILE --PRODUCTNAME=$params.PRODUCT_NAME"
             }
         }
     }
